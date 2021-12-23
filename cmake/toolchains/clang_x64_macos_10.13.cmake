4daecfc1af2c9aa1f4cbc08d689fd2320bbbbcf3
Adam Simmons <hi@adamjsimmons.com>
2021-12-23 13:49:40 -0600

    Update CMake scripts.

--- cmake/toolchains/clang_x64_macos_10.13.cmake
+++ cmake/toolchains/clang_x64_macos_10.13.cmake
@@ -4,8 +4,8 @@ else ()
   set(TOOLCHAIN_MACOS_10_13_CLANG_CMAKE 1)
 endif ()
 
-set(CMAKE_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX10.13.sdk"  CACHE PATH    "(macOS only) The path to the macOS platform SDK.")
-set(CMAKE_OSX_DEPLOYMENT_TARGET "10.13"                                       CACHE STRING  "(macOS only) The minimum macOS deployment target.")
+set(CMAKE_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX10.14.sdk"  CACHE PATH    "(macOS only) The path to the macOS platform SDK.")
+set(CMAKE_OSX_DEPLOYMENT_TARGET "10.14"                                       CACHE STRING  "(macOS only) The minimum macOS deployment target.")
 set(CMAKE_POSITION_INDEPENDENT_CODE CACHE BOOL ON)
 set(PORT "UltralightMac")
 
@@ -13,4 +13,8 @@ if (NOT EXISTS ${CMAKE_SYSROOT})
     message(FATAL_ERROR "macOS Platform SDK not found at the following path: ${CMAKE_SYSROOT}")
 endif ()
 
-set(UL_FFMPEG_ARGS --target-os=darwin --enable-asm --cpu=haswell)
+# Set up FFmpeg args
+set(UL_FFMPEG_ARGS --target-os=darwin --enable-asm --cpu=haswell --sysroot=${CMAKE_SYSROOT})
+
+# Set up GStreamer C flags
+set(UL_GSTREAMER_C_FLAGS --sysroot=${CMAKE_SYSROOT})
